3196207cb844b751aa9dbaa0fb7c62034ce44056
wesfewewfewfw <filippiatkowski@outlook.com>
2022-11-12 19:19:52 +0100

    dodany drugi node który faktycznie mieli dane

--- src/youtube_trending/pipelines/data_processing/nodes.py
+++ src/youtube_trending/pipelines/data_processing/nodes.py
@@ -2,6 +2,11 @@ import json
 import os
 import pandas as pd
 import numpy as np
+import logging
+from pandas import DataFrame
+from typing import Dict
+
+log = logging.getLogger(__name__)
 
 def read_files(datasets_path: str):
     '''Sczytuje csv'ki i JSONy zawierające informacje
@@ -11,12 +16,12 @@ def read_files(datasets_path: str):
     datasets_path: string będący ścieżką do danych
 
     Wyjście:
-    csvs: lista dataframe'ów z dodaną informacją o kraju pochodzenia
+    csvs: słownik gdzie kluczem jest nazwa pliku, a wartością dataframe
     categories: słownik gdzie kluczem jest id kategorii, a wartością nazwa
     '''
     
-    csvs = []
-    categories = {} 
+    csvs = {}
+    categories = {}
 
     for f in os.listdir(datasets_path):
         file_path = datasets_path + "/" + f
@@ -32,13 +37,38 @@ def read_files(datasets_path: str):
                     
         elif f.endswith('.csv'):
             csv = pd.read_csv(file_path, encoding='ISO-8859-1')
-            # Dodaje kolumnę country jako nazwę czytanego pliku
-            csv["country"] = f
-            # Usuwa tę kolumnę, żeby dodać ją jako pierwszą
-            country_col = csv.pop("country")
-            csv.insert(0, "country", country_col)
-            # Dodaje do listy
-            csvs.append(csv)
+            # Dodaje do słownika
+            csvs[f] = csv
     
     return csvs, categories
 
+
+
+
+
+
+
+
+
+
+
+# {1: "Film & Animations", 10: "Music"}
+
+def prepare_trending_data(csvs: Dict[str, DataFrame], categories: Dict[int, str]):
+    '''
+    '''
+    trending_data = {}
+
+    for country, csv in csvs.items():
+        # wzięte z artykułu
+        csv["description"] = csv["description"].fillna(value="")
+
+        # brzydki sposób na zamianę id kategorii na jej nazwę
+        csv["category"] = ""
+        for row in csv.index:
+            csv["category"][row] = categories[int(csv["category_id"][row])]
+            
+        csv.pop("category_id")
+        trending_data[country] = csv
+
+    return trending_data
--- src/youtube_trending/pipelines/data_processing/pipeline.py
+++ src/youtube_trending/pipelines/data_processing/pipeline.py
@@ -1,15 +1,21 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import read_files
+from .nodes import read_files, prepare_trending_data
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
-           node(
+            node(
                 func=read_files,
                 inputs="params:datasets_path",
                 outputs=["csvs", "categories"],
                 name="file_reading_node",
-            )            
+            ),
+            node(
+                func=prepare_trending_data,
+                inputs=["csvs", "categories"],
+                outputs="trending_data",
+                name="data_processing_node"
+            )
         ]
     )
