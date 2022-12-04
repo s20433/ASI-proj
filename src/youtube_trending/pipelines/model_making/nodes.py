2f84fdb131ffba07b5b7bb4de0befddf7fa16f68
wesfewewfewfw <filippiatkowski@outlook.com>
2022-12-04 20:57:04 +0100

    Dodano metodę na tuning parametrów, tylko kedro nie ma skąd wziąć optuna.Trial

--- src/youtube_trending/pipelines/model_making/nodes.py
+++ src/youtube_trending/pipelines/model_making/nodes.py
@@ -7,8 +7,9 @@ import logging
 from pandas import DataFrame
 from typing import Dict
 
+log = logging.getLogger(__name__)
 
-def prepare_data_for_models(trending_data: Dict[str, DataFrame]):
+def prepare_data_for_models(trending_data: Dict[str, DataFrame]): 
     '''Przygotowuje dane, żeby na ich podstawie utworzyć model
     
     ### Wejście:
@@ -28,17 +29,80 @@ def prepare_data_for_models(trending_data: Dict[str, DataFrame]):
         for row in csv.index:
             likes = csv.loc[row, "likes"]
             dislikes = csv.loc[row, "dislikes"]
-#!!!
-#!!!
-#!!!
+
             # Kontrowersyjne: jeśli nie ma łapek w dół, może też mieć mało wyświetleń
             # Więc ciężko stwierdzić, czy stosunek likes/0 faktycznie skutkuje dobrym filmem
             if dislikes == 0:
                 is_good = 0
             else:
                 # filmiki mające stosunek 
-                is_good = 1 if (likes / dislikes) <= 0.57 else 0
+                is_good = 1 if (likes / (likes+dislikes)) >= 0.69 else 0
             y.append(is_good)
-        prepared_data[country] = pd.concat([X, pd.Series(y)], axis=1)
-
+        prepared_data[country] = pd.concat([X, pd.Series(y, name="good_video")], axis=1)
     return prepared_data
+
+import wandb
+from pycaret.classification import setup
+from pycaret.classification import tune_model
+
+def create_models(prepared_data: Dict[str, DataFrame]):
+    '''Wykorzystuje przygotowane dane, żeby za pomocą PyCareta wybrać najlepszy
+    model dla każdego dataframe'a
+
+    ### Wejście:
+    prepared_data: ten sam słownik, ale z df'ami w formacie X i y, gdzie
+        X zawiera cechy filmików na czasie, a
+        y czy film, według stosunku łapek w górę do łapek w dół, jest dobry 
+    
+    ### Wyjście:
+    models: słownik, gdzie kluczem jest nazwa kraju, a wartością wybrany przez PyCareta model
+    '''
+    models = {}
+
+    wandb.init(project="youtube_trending_tracking")
+
+    for country, df in prepared_data.items():
+        model_setup = setup(
+            data=df,
+            target="good_video",
+            log_experiment=True,
+            experiment_name="youtube_trending_tracking",
+            session_id=2137069,
+            verbose=False
+        )
+        models[country] = model_setup.compare_models(
+            include=["dt", "gbc", "ada", "rf", "et", "ridge", "knn"]
+        )
+    return models
+
+import optuna
+from optuna import Trial
+
+def objective(trial: Trial):
+    pass
+
+
+def tune_model_parameters(models, optuna_trial: Trial):
+    '''
+    
+    '''
+    tuned_models = {}
+
+    for country, model in models.items():
+        tuned_model = tune_model(model,
+            round=optuna_trial.suggest_int(f"{country}-tuner-round", low=3, high=6),
+            n_iter=optuna_trial.suggest_int(f"{country}-tuner-iters", low=5, high=50),
+        )
+        tuned_models[country] = tuned_model
+    
+    study = optuna.create_study(
+        storage="sqlite:///yt-trending-trials.db",
+        sampler=optuna.samplers.NSGAIISampler(),
+        pruner=optuna.pruners.SuccessiveHalvingPruner(),
+        study_name="yt-trending-study",
+        direction=optuna.study.StudyDirection.MAXIMIZE,
+        load_if_exists=True
+    )
+    study.optimize(func=objective, n_trials=10, n_jobs=1, show_progress_bar=True)
+    
+    return tuned_models
--- src/youtube_trending/pipelines/model_making/pipeline.py
+++ src/youtube_trending/pipelines/model_making/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.18.3
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import prepare_data_for_models
+from .nodes import prepare_data_for_models, create_models, tune_model_parameters
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -14,5 +14,17 @@ def create_pipeline(**kwargs) -> Pipeline:
             inputs="trending_data",
             outputs="prepared_data",
             name="data_preparing_node",
-        )
+        ),
+        node(
+            func=create_models,
+            inputs="prepared_data",
+            outputs="models",
+            name="pycaret_model_finder_node"
+        )#,
+        #node(
+        #    func=tune_model_parameters,
+        #    inputs=["models", "params:optuna_trial"],
+        #    outputs="tuned_models",
+        #    name="optuna_model_tuning_node"
+        #)
     ])
